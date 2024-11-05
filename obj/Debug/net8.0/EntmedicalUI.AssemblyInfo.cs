770c928437607726d36653f56e56ff0189ebd526
Muhammadjon <rahmatullayevmuhammadjon042@gmail.com>
2024-11-05 16:18:38 +0500

    Diagnostika component added

--- obj/Debug/net8.0/EntmedicalUI.AssemblyInfo.cs
+++ obj/Debug/net8.0/EntmedicalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntmedicalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23a3df2638c1c9d95916564671ee841d38e2d69e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40e2f6b938a8cb28903ed6d91c5fd13f9fd13763")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntmedicalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntmedicalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
